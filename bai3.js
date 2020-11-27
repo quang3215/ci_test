56025c91cf5d55793402c2481871a1621731c370
lê minh quang <72089951+quang3215@users.noreply.github.com>
2020-11-27 22:13:05 +0700

    Add files via upload

--- bai3.js
+++ bai3.js
@@ -1,5 +1,11 @@
 let input = document.querySelector('.a');
 let button = document.querySelector('.b');
+let number = 5;
+let i = 0;
 button.onclick = function() {
-    console.log(input.value)
+    if (input.value == 5) {
+        alert('bạn đã đoán đúng')
+    } else {
+        alert("bạn đã đoán sai mời bạn nhập lại");
+    }
 }
